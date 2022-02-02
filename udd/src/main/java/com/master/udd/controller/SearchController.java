e1f71263cfc3eea6bf409ebb459ce436a4da3a19
Eva Janković <evaj.evadesign@gmail.com>
2022-02-02 03:48:45 +0100

    Initial logger and ELK stack configuration
    - ip address hard coded on backend, should be fetched server/client side from one of the APIs

--- udd/src/main/java/com/master/udd/controller/SearchController.java
+++ udd/src/main/java/com/master/udd/controller/SearchController.java
@@ -3,13 +3,8 @@ package com.master.udd.controller;
 import com.master.udd.dto.SearchRequest;
 import com.master.udd.dto.SearchResponse;
 import com.master.udd.exception.InvalidAddressException;
-import com.master.udd.lucene.model.CVIndex;
 import com.master.udd.lucene.service.SearchService;
-import com.master.udd.model.Location;
-import com.master.udd.service.LocationService;
 import lombok.AllArgsConstructor;
-import org.apache.coyote.Response;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -20,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api/search")
@@ -30,10 +27,14 @@ public class SearchController {
 
     private SearchService searchService;
 
+    private final static Logger LOGGER = Logger.getLogger("SearchController.class");
+
     @GetMapping("/basic")
     public ResponseEntity<Page<SearchResponse>> basicSearch(
             Pageable pageable,
-            String query) {
+            String query,
+            HttpServletRequest request) {
+        LOGGER.info("query=[" + query + "] ip=90.116.164.204");
         List<SearchResponse> foundCvs = searchService.basicSearch(query, pageable);
         return new ResponseEntity<>(
                 new PageImpl<>(foundCvs, pageable, foundCvs.size()),
